edd85f70373728649bd7f26c0ee62445d8a5638f
Developer_Rahul <sahrahul0801@gmail.com>
2024-02-05 22:34:28 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollToTopButton = document.getElementById("scroll-to-top");
+
+    // Add a click event listener to the button
+    scrollToTopButton.addEventListener("click", function() {
+        // Scroll to the top of the document
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // Use smooth scrolling if supported
+        });
+    });
+
+    // Show/hide the button based on scroll position
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 100) {
+            scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+        }
+    });
+});
