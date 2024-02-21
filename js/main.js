8d3e62a4db984194d572ca225731b4ad438abe27
Developer_Rahul <sahrahul0801@gmail.com>
2024-02-21 12:21:25 +0530

    added pre-loader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// preloader
+$(document).ready(function() {
+    setTimeout(function() {
+      $('#container').addClass('loaded');
+      // Once the container has finished, the scroll appears
+      if ($('#container').hasClass('loaded')) {
+        // It is so that once the container is gone, the entire preloader section is deleted
+        $('#preloader').delay(9000).queue(function() {
+          $(this).remove();
+        });}
+    }, 3000);});
+
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
     var scrollToTopButton = document.getElementById("scroll-to-top");
